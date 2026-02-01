aa6a062d415f8fcf72a5359d7901a64d42ff1596
SagarNikam09 <sagarpatilx388@gmail.com>
2026-02-01 08:54:26 +0530

    feat: end-to-end Glue ETL with Terraform and CI/CD

---
+++ terraform/glue_core.tf
@@ -0,0 +1,100 @@
+# Glue database
+resource "aws_glue_catalog_database" "citibike_db" {
+  name        = "${var.project_name}_${var.environment}_db"
+  description = "CitiBike analytics database"
+}
+
+# Job 1: Raw → Silver
+resource "aws_glue_job" "job1_bronze_to_silver" {
+  name     = "${var.project_name}-${var.environment}-job1-bronze-to-silver"
+  role_arn = aws_iam_role.glue_role.arn
+
+  command {
+    name           = "glueetl"
+    python_version = "3"
+    # CHANGE script key if your script name/folder differs
+    script_location = "s3://${var.s3_bucket_name}/glue_scripts/raw_to_silver.py"
+  }
+
+  glue_version      = "4.0"
+  number_of_workers = 4
+  worker_type       = "G.1X"
+  timeout           = 60
+
+  default_arguments = {
+    "--job-bookmark-option"              = "job-bookmark-enable"
+    "--enable-metrics"                   = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-spark-ui"                  = "true"
+    "--TempDir"                          = "s3://${var.s3_bucket_name}/temp/"
+    "--raw_path"                         = "s3://${var.s3_bucket_name}/data/raw/citibike/"
+    "--silver_path"                      = "s3://${var.s3_bucket_name}/data/silver/citibike/"
+    "--database_name"                    = aws_glue_catalog_database.citibike_db.name
+  }
+}
+
+# Job 2: Silver → Gold
+resource "aws_glue_job" "job2_silver_to_gold" {
+  name     = "${var.project_name}-${var.environment}-job2-silver-to-gold"
+  role_arn = aws_iam_role.glue_role.arn
+
+  command {
+    name           = "glueetl"
+    python_version = "3"
+    # CHANGE script key if your script name/folder differs
+    script_location = "s3://${var.s3_bucket_name}/glue_scripts/silver_to_gold.py"
+  }
+
+  glue_version      = "4.0"
+  number_of_workers = 8
+  worker_type       = "G.1X"
+  timeout           = 60
+
+  default_arguments = {
+    "--job-bookmark-option"              = "job-bookmark-enable"
+    "--enable-metrics"                   = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-spark-ui"                  = "true"
+    "--TempDir"                          = "s3://${var.s3_bucket_name}/temp/"
+    "--silver_path"                      = "s3://${var.s3_bucket_name}/data/silver/citibike/"
+    "--gold_path"                        = "s3://${var.s3_bucket_name}/data/gold/citibike/"
+    "--database_name"                    = aws_glue_catalog_database.citibike_db.name
+  }
+}
+
+# Crawlers
+resource "aws_glue_crawler" "raw_data_crawler" {
+  name          = "${var.project_name}-${var.environment}-raw-crawler"
+  role          = aws_iam_role.glue_role.arn
+  database_name = aws_glue_catalog_database.citibike_db.name
+
+  s3_target {
+    path = "s3://${var.s3_bucket_name}/data/raw/citibike/"
+  }
+
+  table_prefix = "raw_"
+}
+
+resource "aws_glue_crawler" "silver_crawler" {
+  name          = "${var.project_name}-${var.environment}-silver-crawler"
+  role          = aws_iam_role.glue_role.arn
+  database_name = aws_glue_catalog_database.citibike_db.name
+
+  s3_target {
+    path = "s3://${var.s3_bucket_name}/data/silver/citibike/"
+  }
+
+  table_prefix = "silver_"
+}
+
+resource "aws_glue_crawler" "gold_crawler" {
+  name          = "${var.project_name}-${var.environment}-gold-crawler"
+  role          = aws_iam_role.glue_role.arn
+  database_name = aws_glue_catalog_database.citibike_db.name
+
+  s3_target {
+    path = "s3://${var.s3_bucket_name}/data/gold/citibike/"
+  }
+
+  table_prefix = "gold_"
+}
